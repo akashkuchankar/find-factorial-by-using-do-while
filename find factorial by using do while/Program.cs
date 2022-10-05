c9db7e950d78d38f570ec1191e50883054287fe9
akashkuchankar <kuchankarakash@gmail.com>
2022-10-05 11:13:35 -0700

    Add project files.

---
+++ find factorial by using do while/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_factorial_by_using_do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5;
+            int F = 1;
+            {
+                int i = 1;
+                do
+                {
+                    F = F * i;
+                  //  Console.WriteLine(F);
+                    i++;
+                } while (i <= N);
+
+            }
+            Console.WriteLine(F);
+            
+             
+        }
+    }
+}
